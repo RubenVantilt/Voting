4406adb3be8d2c04f024f777cf73d1c9ad9d4372
Vantilt Ruben <ruben.vantilt@cegeka.com>
2017-12-12 16:16:14 +0100

    initial commit

---
+++ Voting/Voting.Interfaces/IVotingService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.ServiceFabric.Services.Remoting;
+
+namespace Voting.Interfaces
+{
+    public interface IVotingService : IService
+    {
+        Task AddOption(string option);
+
+        Task Vote(string option);
+    }
+}
+
---
+++ Voting/Voting.WebAPI/Controllers/VotingController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.ServiceFabric.Services.Remoting.Client;
+using Voting.Interfaces;
+
+
+namespace Voting.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    public class VotingController : Controller
+    {
+        public VotingController()
+        {
+            
+        }
+
+        [HttpPost]
+        [Route("AddOption")]
+        public async Task<IActionResult> AddOption([FromBody] string option)
+        {
+            var proxy = ServiceProxy.Create<IVotingService>(new Uri("fabric:/Voting/VotingService"));
+
+            await proxy.AddOption(option).ConfigureAwait(false); 
+
+            return Ok();
+        }
+
+        [HttpPost]
+        [Route("Vote")]
+        public async Task<IActionResult> Vote([FromBody] string option)
+        {
+            var proxy = ServiceProxy.Create<IVotingService>(new Uri("fabric:/Voting/VotingService"));
+
+            await proxy.Vote(option).ConfigureAwait(false);
+
+            return Ok();
+        }
+    }
+}
